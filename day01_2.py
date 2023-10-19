29282ad57bd29aa1512a7a687f1f7dc431d9fdce
Sky_Interest <75404389+Sky-Interest@users.noreply.github.com>
2023-10-19 23:43:44 +0800

    Add files via upload

---
+++ day01_2.py
@@ -0,0 +1,9 @@
+"""
+    使用公式求出圆的周长
+    作者 : Sky_Interest
+    时间 : 23.8.31
+"""
+length = 3
+width = 5
+area = length*width
+print(area)
---
+++ day01_3.py
@@ -0,0 +1,10 @@
+"""
+    练习
+    作者 : Sky_Interest
+    时间 : 23.8.31
+"""
+length = 3
+width = 5
+area = length*width
+if area > 10:
+    print("大矩形")
